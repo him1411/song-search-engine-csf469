45a23e72de39feae56f68d2d6d295fe4c66a6279
SUBHADIP JANA <subhadip1997a@gmail.com>
2017-09-23 21:04:19 +0530

    modified extractDocs

--- extractDocs.py
+++ extractDocs.py
@@ -5,16 +5,19 @@ def createDocumentForQuestion(docNum):
     site = open('w.html')
 
     stackSoup = bs4.BeautifulSoup(site.read(),"lxml")
+    text = stackSoup.select('p.label-key')
+    label = text[1].getText() + '_' + text[3].getText()[40:text[3].getText().find('t')]
     text = stackSoup.select('div.post-text p')
+    label = label + '_' + str(len(text))
     fullText = ''
     for i in range(0,len(text)):
         fullText = fullText + text[i].getText()
-    with open( str(docNum)+ '.html','w') as fid:
+    with open( str(docNum) + '_' + label + '_' + '.html','w') as fid:
         fid.write(fullText)
 
 
 i=0
-with open('linksCorpus2.txt') as f:
+with open('linksCorpus.txt') as f:
     for line in f:
         i = i + 1
         http = urllib3.PoolManager()
@@ -23,3 +26,4 @@ with open('linksCorpus2.txt') as f:
         with open('w.html', 'w') as fid:
             fid.write(str(question_page.data))
         createDocumentForQuestion(str(i))
+
