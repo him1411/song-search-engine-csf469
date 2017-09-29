3dd2e9e69974b1a210ec86972c3316d5edf926c5
SUBHADIP JANA <subhadip1997a@gmail.com>
2017-09-30 03:53:01 +0530

    Added idf and tdf in combined3.py

--- combined3.py
+++ combined3.py
@@ -1,50 +1,91 @@
 from math import log
 import nltk
 from nltk import word_tokenize
+from nltk import FreqDist
 import sys
-import optparse
-import logging
+import math
 import os
 from nltk.stem.snowball import SnowballStemmer
 from collections import defaultdict
 import pickle
-from os import walk
 
 vocabulary = {}
-document_tokens_list[]
-docFiles = [f for f in os.listdir('.') if os.path.isfile(f)]
+vocabulary_idf = {}
+freqDist = {}
+document_tokens_list= []
+temp_doc_tokens = []
+snowball_stemmer = SnowballStemmer('english')
+docFiles = [f for f in os.listdir('./Documents') if f.endswith(".txt")]
 
 def __build_vocabulary(document_tokens):
         vocabulary_index=len(vocabulary)-1
-            for word in document_tokens:
+        for word in document_tokens:
+                if word not in vocabulary_idf:
+                    vocabulary_idf[word] = 1
+                else:
+                    vocabulary_idf[word] = vocabulary_idf[word] + 1
+
                 if word not in vocabulary:
                             vocabulary[word] = vocabulary_index
                             vocabulary_index+= 1
 
-document_tokens1 = [word for sent in nltk.sent_tokenize((getText("HP1.txt")).decode('utf-8')) for word in nltk.word_tokenize(sent)]
-document_tokens1 = [stemmer.stem(t) for t in document_tokens1]
-document_tokens1 = [stemmer.stem(t) for t in document_tokens1 if t not in nltk.corpus.stopwords.words('english')]
-document_tokens_list=[document_tokens1]
+def buildIDF():
+    for word in vocabulary:
+        for document_tokens in document_tokens_list:
+            if word in document_tokens:
+                if word in vocabulary_idf:
+                    vocabulary_idf[word] = vocabulary_idf[word] + 1
+                else:
+                    vocabulary_idf[word] = 1
 
-for file in list:
-    file_name="./Documents/"+file
-    __build_vocabulary(file_name)
+def buildFreqDist(document_tokens_list):
+    i=0
+    for document_tokens in document_tokens_list:
+        freqDist[i] = FreqDist(document_tokens)
+        i = i + 1
+        for word in document_tokens:
+            vocabulary_idf
+
+def returnTermFrequency(term, document_tokens, document_tokens_index):
+    return math.log2(1+(freqDist[document_tokens_index][term]/float(len(document_tokens))))
+
+def returnIdf(term):
+    return math.log2(len(document_tokens_list)/vocabulary_idf[term])
+
+#****************************************************************************************************************************#
+
+for file in docFiles:
+    print(file)
+    file_name = open("./Documents/"+file)
+    words = file_name.read()
+    temp_doc_tokens = nltk.word_tokenize(words)
+    temp_doc_tokens = [w.lower() for w in temp_doc_tokens]
+    temp_doc_tokens = [snowball_stemmer.stem(token) for token in temp_doc_tokens]
+    temp_doc_tokens = [snowball_stemmer.stem(token) for token in temp_doc_tokens if token not in nltk.corpus.stopwords.words('english')]
+    document_tokens_list.append(temp_doc_tokens)
+    print(temp_doc_tokens[:20])
 
 for document_tokens in document_tokens_list:
     __build_vocabulary(document_tokens)
 print (vocabulary)
-
+buildFreqDist(document_tokens_list)
+buildIDF()
+print(vocabulary_idf)
 j=0
+k=0;
 primaryDictionary = dict()
-print (len(document_tokens_list))
 
 for vocab in vocabulary:
     j+=1
     if vocab not in primaryDictionary:
         inner_dict=dict()
-        for i in range(len(document_tokens_list)):
-            inner_dict[i]=dict()
-            inner_dict[i]=(tf_idf_rapport(vocab,document_tokens_list[i],document_tokens_list))
+        k=0
+        for document_tokens in document_tokens_list:
+            inner_dict[k]=dict()
+            termFreq = returnTermFrequency(vocab, document_tokens, k)
+            idf = returnIdf(vocab)
+            inner_dict[k] = {1:termFreq,2:idf,3:(termFreq*idf)}
+            k = k + 1
+            #inner_dict[i]=(tf_idf_rapport(vocab,document_tokens_list[i],document_tokens_list))
     primaryDictionary[vocab]=inner_dict
-
-print (primaryDictionary)
+#print (primaryDictionary)
