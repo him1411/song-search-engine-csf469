0925c370a1e1972c49d4c20748ff273ca46d23f9
him1411 <himjan1416@gmail.com>
2017-11-18 16:20:40 +0530

    removed large files

--- earlier stuff/Page.py
+++
@@ -1,9 +0,0 @@
-import bs4, sys
-
-with open(sys.argv[1], 'r') as f:
-    webpage = f.read().decode('utf-8')
-
-soup = bs4.BeautifulSoup(webpage)
-for node in soup.findAll('html'):
-    print u''.join(node.findAll(text=True)).encode('utf-8')
-
--- earlier stuff/README.md
+++
@@ -1,2 +0,0 @@
-# ir-assignment-csf469
-assignments of the information retrieval course  
